aed665d1d021d971f2c1afd2ed504a973f5c5ed2
Gareth Nunns <garethnunns@gmail.com>
2017-04-23 23:22:21 +0100

    Favicon
    Simple addition

--- Web/site/head.php
+++ Web/site/head.php
@@ -1,6 +1,27 @@
 <link rel="stylesheet" type="text/css" href="/site/memes.css?version=<?php echo time(); // remove when out of dev ?>">
 <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
 
+<!-- favicons -->
+<!-- For iPad with high-resolution Retina display running iOS ≥ 7: -->
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo $web; ?>favicon/favicon-152x152.png">
+<!-- For iPad with high-resolution Retina display running iOS ≤ 6: -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo $web; ?>favicon/favicon-144x144.png">
+<!-- For iPhone with high-resolution Retina display running iOS ≥ 7: -->
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo $web; ?>favicon/favicon-128x128.png">
+<!-- For iPhone with high-resolution Retina display running iOS ≤ 6: -->
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo $web; ?>favicon/favicon-114x114.png">
+<!-- For first- and second-generation iPad: -->
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo $web; ?>favicon/favicon-72x72.png">
+<!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+<link rel="apple-touch-icon-precomposed" href="<?php echo $web; ?>favicon/favicon-57x57.png">
+<!-- Windows tile -->
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?php echo $web; ?>favicon/favicon-114x114.png">
+<!-- Most browsers -->
+<link rel="icon" href="<?php echo $web; ?>favicon.ico">
+<link rel="icon" href="<?php echo $web; ?>favicon/favicon-32x32.png" sizes="32x32">
+<!--[if IE]><link rel="shortcut icon" href="http://altiview.co.uk/wp-content/themes/altiview/favicon.ico"><![endif]-->
+
 <script
 	src="https://code.jquery.com/jquery-3.2.1.min.js"
 	integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
