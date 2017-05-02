af0ace1b010e78722592b1641faf0a5504741f7f
Gareth Nunns <garethnunns@gmail.com>
2017-05-02 14:41:37 +0100

    Header tweak
    Now uses userDetailsPersonal and a more useful variable name

--- Web/site/header.php
+++ Web/site/header.php
@@ -5,11 +5,11 @@
 
 <?php
 	if(loggedIn()) {
-		$user = userDetails($_SESSION['key']);
+		$header = userDetailsPersonal($_SESSION['key'],$_SESSION['user']);
 		// when the user is logged in these buttons will display
 ?>
 		<div class="buttons">
-			<?php echo '<a href="/'.$user->username.'" class="pp '. ($_SERVER['REQUEST_URI'] == "/{$user->username}" ? 'current': '') .'"><img src="'.$res.$user->picUri.'" alt="'.$user->username.' profile picture" /></a>'; ?>
+			<?php echo '<a href="'.$header['profile']['link'].'" class="pp '. ($_SERVER['REQUEST_URI'] == "/{$header['profile']['username']}" ? 'current': '') .'"><img src="'.$header['profile']['pic'].'" alt="'.$header['profile']['username'].' profile picture" /></a>'; ?>
 			<a href="/starred" class="icon-star-full <?php if($_SERVER['REQUEST_URI'] == '/starred/') echo 'current' ?>" title="Starred"></a>
 			<a href="/settings" class="icon-settings <?php if($_SERVER['REQUEST_URI'] == '/settings/') echo 'current' ?>" title="Settings"></a>
 			<a href="/logout" class="icon-logout" title="Logout"></a>
